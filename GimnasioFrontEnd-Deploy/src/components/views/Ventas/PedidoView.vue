1248d1bd670d96feb6ea1dc7c0f9ea7f26dffb5b
Edgar <210519@utxicotepec.edu.mx>
2024-08-19 12:10:31 -0600

    Agregación de vistas a mis entidades

--- GimnasioFrontEnd-Deploy/src/components/views/Ventas/PedidoView.vue
+++ GimnasioFrontEnd-Deploy/src/components/views/Ventas/PedidoView.vue
@@ -1,48 +1,172 @@
 <template>
-    <form>
-        <h1 class="text-2xl xl:text-3xl font-extrabold">Agregar Pedido</h1>
+    <h1 class="title-gym text-center">Estatus de pedidos</h1>
+        <section class="m-4">
+            <table class="w-full bg-white text-left text-sm text-gray-900 rounded ">
+                <thead class="bg-gray-50 text-center">
+                    <tr>
+                        <th scope="col" class="px-6 py-4 bg-gray-900 font-medium text-gray-100 rounded-l-md">Nombre de Producto</th>
+                        <th scope="col" class="px-6 py-4 bg-gray-900 font-medium text-gray-100">Cantidad</th>
+                        <th scope="col" class="px-6 py-4 bg-gray-900 font-medium text-gray-100">Precio</th>
+                        <th scope="col" class="px-6 py-4 bg-gray-900 font-medium text-gray-100">Fecha del pedido</th>
+                        <th scope="col" class="px-6 py-4 bg-gray-900 font-medium text-gray-100">Estatus</th>
+                        <th scope="col" class="px-6 py-4 bg-gray-900 font-medium text-gray-100">Aplica en</th>
+                        <th scope="col" class="px-6 py-4 bg-gray-900 font-medium text-gray-100 rounded-r-md"></th>
+                    </tr>
+                </thead>
+                <tbody class="bg-gray-200">
+                    <tr class="hover:bg-gray-500 ">
+                        <th class="h-[50px] text-center">
+                            <span>Proteína Whey</span>
+                        </th>
+                        <th class="h-[50px] text-center">
+                            <span>3</span>
+                        </th>
+                        <th class="text-center">
+                            <span>$1760.60</span>
+                        </th>
+                        <th class="text-center">
+                            <span>03-Agosto-2024</span>
+                        </th>
+                        <th class="text-center">
+                            <span>Proceso</span>
+                        </th>
+                        <th class="text-center">
+                            <span>Pomoción</span>
+                        </th>
+                        <th class="flex justify-center">
+                            <button type="button" id="producto" class="px-4 mt-1 py-2 rounded-md bg-red-600 text-white hover:bg-gray-600">
+                                Editar Pedido
+                            </button>
+                        </th>
+                    </tr>
 
-        <input
-            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 col-span-2"
-            type="text" placeholder="Tipo"
-        />
-        <input
-            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-            type="date" placeholder="Fecha registro"
-        />
-        <input
-            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-            type="date" placeholder="Fecha actualizacion"
-        />
-        <select id="countries" class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5">
-            <option selected>Estatus</option>
-            <option value="M">Inactivo</option>
-            <option value="F">Activo</option>
-        </select>
-        <input
-            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-            type="text" placeholder="Total de productos"
-        />
-        <input
-            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-            type="text" placeholder="Costo Total"
-        />
-    
-    
-        <a href="/" class="col-span-2" >
-            <button
-                class="mt-5 tracking-wide font-semibold bg-red-700 text-gray-100 w-full py-4 rounded-lg hover:bg-red-900 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none col-span-2">
-                <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
-                    stroke-linecap="round" stroke-linejoin="round">
-                    <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
-                    <circle cx="8.5" cy="7" r="4" />
-                    <path d="M20 8v6M23 11h-6" />
-                </svg>
-                <span class="ml-3 uppercase">
-                    Agregar Pedidos
-                </span>
-            </button>
-        </a>
-        <br>
-    </form>
+                    <tr class="hover:bg-gray-500">
+                        <th class="h-[50px] text-center">
+                            <span>Creatina Monohidratada</span>
+                        </th>
+                        <th class="h-[50px] text-center">
+                            <span>4</span>
+                        </th>
+                        <th class="text-center">
+                            <span>$2400.00</span>
+                        </th>
+                        <th class="text-center">
+                            <span>04-Agosto-2024</span>
+                        </th>
+                        <th class="text-center">
+                            <span>Iniciado</span>
+                        </th>
+                        <th class="text-center">
+                            <span>Precio tienda</span>
+                        </th>
+                        <th class="flex justify-center">
+                            <button type="button" id="producto" class="px-4 mt-1 py-2 rounded-md bg-red-600 text-white hover:bg-gray-600">
+                                Editar Pedido
+                            </button>
+                        </th>
+                    </tr>
+
+                    <tr class="hover:bg-gray-500">
+                        <th class="h-[50px] text-center">
+                            <span>BCAA</span>
+                        </th>
+                        <th class="h-[50px] text-center">
+                            <span>2</span>
+                        </th>
+                        <th class="text-center">
+                            <span>$1040.00</span>
+                        </th>
+                        <th class="text-center">
+                            <span>05-Agosto-2024</span>
+                        </th>
+                        <th class="text-center">
+                            <span>Entregado</span>
+                        </th>
+                        <th class="text-center">
+                            <span>Promoción</span>
+                        </th>
+                        <th class="text-center">
+                            <span>24-Noviembre-2024</span>
+                        </th>
+                    </tr>
+
+                    <tr class="hover:bg-gray-500">
+                        <th class="h-[50px] text-center">
+                            <span>Pre-entrenador</span>
+                        </th>
+                        <th class="h-[50px] text-center">
+                            <span>7</span>
+                        </th>
+                        <th class="text-center">
+                            <span>$2800.00</span>
+                        </th>
+                        <th class="text-center">
+                            <span>06-Agosto-2024</span>
+                        </th>
+                        <th class="text-center">
+                            <span>Iniciado</span>
+                        </th>
+                        <th class="text-center">
+                            <span>Precio tienda</span>
+                        </th>
+                        <th class="flex justify-center">
+                            <button type="button" id="producto" class="px-4 mt-1 py-2 rounded-md bg-red-600 text-white hover:bg-gray-600">
+                                Editar Pedido
+                            </button>
+                        </th>
+                    </tr>
+
+                    <tr class="hover:bg-gray-500">
+                        <th class="h-[50px] text-center">
+                            <span>Multivitamínico</span>
+                        </th>
+                        <th class="h-[50px] text-center">
+                            <span>12</span>
+                        </th>
+                        <th class="text-center">
+                            <span>$2400.00</span>
+                        </th>
+                        <th class="text-center">
+                            <span>03-Agosto-2024</span>
+                        </th>
+                        <th class="text-center">
+                            <span>Proceso</span>
+                        </th>
+                        <th class="text-center">
+                            <span>Promoción</span>
+                        </th>
+                        <th class="flex justify-center">
+                            <button type="button" id="producto" class="px-4 mt-1 py-2 rounded-md bg-red-600 text-white hover:bg-gray-600">
+                                Editar Pedido
+                            </button>
+                        </th>
+                    </tr>
+
+                    <tr class="hover:bg-gray-500">
+                        <th class="h-[50px] text-center">
+                            <span>Proteína Vegana</span>
+                        </th>
+                        <th class="h-[50px] text-center">
+                            <span>9</span>
+                        </th>
+                        <th class="text-center">
+                            <span>$4500.00</span>
+                        </th>
+                        <th class="text-center">
+                            <span>04-Agosto-2024</span>
+                        </th>
+                        <th class="text-center">
+                            <span>Entregado</span>
+                        </th>
+                        <th class="text-center">
+                            <span>Precio tienda</span>
+                        </th>
+                        <th class="text-center">
+                            <span>10-Agosto-2024</span>
+                        </th>
+                    </tr>
+                </tbody>
+            </table>
+            <hr class="border-2 mt-4">
+        </section>
 </template>
